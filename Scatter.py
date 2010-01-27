0f5a50e0abfd3340720046820b1017bfb14c46f0
Alex Rasmussen <alexras@acm.org>
2010-01-26 22:47:21 -0800

    Give scatter plots a larger default marker size.

--- Scatter.py
+++ Scatter.py
@@ -6,6 +6,7 @@ class Scatter(PlotInfo):
     def __init__(self):
         PlotInfo.__init__(self, "scatter")
         self.marker = 'o'
+        self.markerSize = 20
         self.color = "black"
             
     def draw(self, axis):
@@ -13,7 +14,8 @@ class Scatter(PlotInfo):
         kwdict["marker"] = self.marker
         kwdict["label"] = self.label
         kwdict["color"] = self.color
-
+        kwdict["s"] = self.markerSize
+        
         return [[axis.scatter(self.xValues, self.yValues, **kwdict)], 
                 [self.label]]
     
