822c52a8a5cc8f9997f509df341b00c96def0b3e
Alex Rasmussen <alexras@acm.org>
2010-09-01 17:18:24 -0700

    More interesting error bars example.

--- examples/errorbars.py
+++ examples/errorbars.py
@@ -6,26 +6,25 @@ plot = Plot()
 
 # Uneven error bars
 line = Line()
-line.xValues = range(6)
-line.yValues = [25, 21, 30, 23, 10, 30]
-line.yMins = [10, 18, 10, 10, 5, 20]
-line.yMaxes = [30, 50, 40, 30, 20, 45]
+line.xValues = [6,10,4,0,8,2,12]
+line.yValues = [50,90,30,10,70,20,110]
+line.yMins   = [y - 30 for y in line.yValues]
+line.yMaxes  = [y + 50 for y in line.yValues]
 line.label = "Asymmetric Errors"
 line.color = "red"
 
-line.xValues = range(len(line.yValues))
-
 # Even error bars
 line2 = Line()
-line2.xValues = range(6)
-line2.yValues = [35, 40, 45, 40, 55, 50]
+line2.xValues = [1,5,3,9,7,11]
+line2.yValues = [100, 120, 110, 140, 130, 150]
 line2.color = "blue"
 line2.label = "Symmetric Errors"
-line2.yErrors = [3, 6, 5, 3, 5, 4]
+line2.yErrors = [5,25,15,45,35,55]
 
 plot.add(line)
 plot.add(line2)
 plot.setXLabel("X Label")
 plot.setYLabel("Y Label")
 plot.hasLegend()
+plot.setXLimits(-1, 13)
 plot.save("errorbars.png")
