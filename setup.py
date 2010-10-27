40eccc500c2ab67c5780c32f4c2d75a7aa6265ad
Nathan Farrington <nfarring@gmail.com>
2010-10-27 02:35:01 -0700

    Updated setup.py with classifiers and requires.

--- setup.py
+++ setup.py
@@ -7,9 +7,19 @@ setup(name='boomslang',
         description='A thin layer over matplotlib that simplifies creation of common plots',
         author='Alex C. Rasmussen',
         author_email='alexras@acm.org',
-        url='http://code.google.com/p/boomslang/',
+        url='http://code.google.com/p/boomslang/downloads/detail?name=boomslang-0.4b.tar.gz&can=2&q=',
+        classifiers = [f.strip() for f in """
+                     Development Status :: 4 - Beta
+                     Intended Audience :: Developers
+                     License :: OSI Approved :: BSD License
+                     Natural Language :: English
+                     Operating System :: OS Independent
+                     Programming Language :: Python 2
+                     Topic :: Software Development :: Libraries :: Python Modules
+                     """.splitlines() if f.strip()],
+        license='BSD',
+        requires = ['matplotlib'],
         packages=['boomslang'],
-        package_dir={'boomslang':''},
-        license='BSD')
+        package_dir={'boomslang':''})
 
 # vim: set ts=4 sw=4 expandtab:
