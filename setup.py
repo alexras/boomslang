a6074db05e5d8755a3d80d9237ac582dd2471367
Nathan Farrington <nfarring@gmail.com>
2010-10-27 02:50:49 -0700

    Updated the setup.py file with more classifiers. Added a README file.

--- setup.py
+++ setup.py
@@ -3,23 +3,32 @@
 from distutils.core import setup
 
 setup(name='boomslang',
-        version='0.4b',
-        description='A thin layer over matplotlib that simplifies creation of common plots',
-        author='Alex C. Rasmussen',
-        author_email='alexras@acm.org',
-        url='http://code.google.com/p/boomslang/downloads/detail?name=boomslang-0.4b.tar.gz&can=2&q=',
-        classifiers = [f.strip() for f in """
+      version='0.4b',
+      download_url='http://boomslang.googlecode.com/files/boomslang-0.4b.tar.gz',
+      description='A thin layer over matplotlib that simplifies creation of common plots',
+      long_description='''I created Boomslang to decrease the amount of boilerplate
+code I had to write when producing graphs for research papers. Boomslang
+treats data and plots separately and encapsulates them both in objects,
+giving the programmer the ability to author modular, re-usable graphing
+code.'''
+      author='Alex C. Rasmussen',
+      author_email='alexras@acm.org',
+      url='http://code.google.com/p/boomslang/',
+      keywords=['matplotlib'],
+      classifiers = [f.strip() for f in """
                      Development Status :: 4 - Beta
+                     Environment :: Other Environment
                      Intended Audience :: Developers
                      License :: OSI Approved :: BSD License
                      Natural Language :: English
                      Operating System :: OS Independent
-                     Programming Language :: Python 2
+                     Programming Language :: Python
+                     Programming Language :: Python :: 2.5
                      Topic :: Software Development :: Libraries :: Python Modules
                      """.splitlines() if f.strip()],
-        license='BSD',
-        requires = ['matplotlib'],
-        packages=['boomslang'],
-        package_dir={'boomslang':''})
+      license='BSD',
+      requires = ['matplotlib'],
+      packages=['boomslang'],
+      package_dir={'boomslang':''})
 
 # vim: set ts=4 sw=4 expandtab:
