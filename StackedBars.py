029de4d32a00fa7b4a20cbeb93de238d7868924a
Alex Rasmussen <alexras@acm.org>
2010-11-20 15:25:50 -0800

    Realizing the change described in r99, but without breaking transform functionality. Also, cleaned up the implementation a little bit.

--- StackedBars.py
+++ StackedBars.py
@@ -21,59 +21,56 @@ class StackedBars(PlotInfo):
         if not isinstance(bar, Bar):
             print >>sys.stderr, "Can only add Bars to a StackedBars"
             sys.exit(1)
-        
-        if len(self.bars) == 0:
-            # Fake having xValues
-            self.xValues = bar.xValues
-            self.yValues = bar.yValues
+                    
         bar.width = self.width
         self.bars.append(bar)
 
-    def getXLabelLocations(self):
-        if len(self.bars) == 0:
-            return []
-        else:
-            numBarVals = len(self.bars[0].xValues)
-            return range(numBarVals)
-
     def draw(self, axis, transform=None):
-        self.xTickLabelPoints = self.getXLabelLocations()
-
+        if len(self.bars) == 0:
+            return [[], []]
+        
+        self.xValues = [i + i * self.spacing for i in 
+                        xrange(len(self.bars[0].xValues))]
+        if transform:
+            self.xValues = [transform.transform((x,0))[0] for x in self.xValues]
+        
+        self.yValues = self.bars[0].yValues
+        
+        # If the StackedBars doesn't have X label properties set, set them to
+        # the value of the first bar.
+        
+        if not self.xTickLabels:
+            self.xTickLabels = self.bars[0].xTickLabels
+            
+        if not self.xTickLabelPoints:
+            self.xTickLabelPoints = self.bars[0].xTickLabelPoints
+        
+        if len(self.xTickLabelProperties) == 0:
+            self.xTickLabelProperties = self.bars[0].xTickLabelProperties
+            
         super(StackedBars, self).draw(axis)
-
+        
         return self._draw(axis, transform)
-
-    def _draw(self, axis, transform=None):
-        self.xTickLabelPoints = self.getXLabelLocations()
-
+    
+    def _draw(self, axis, transform=None):        
         plotHandles = []
         plotLabels = []
-
-        if len(self.bars) == 0:
-            return [plotHandles, plotLabels]
-
-        numBars = len(self.bars)
         
-        bottoms = [0 for i in xrange(len(self.xTickLabelPoints))]
+        bottoms = [0 for i in xrange(len(self.xValues))]
         
-        xVals = [i + i * self.spacing \
-                     for i in xrange(len(self.bars[0].xValues))]
-
-        if transform:
-            xVals = [transform.transform((x,0))[0] for x in xVals]
-
         for bar in self.bars:
             attrs = bar.getAttributes()
             attrs['width'] = self.width
-
-            currHandle = axis.bar(xVals, bar.yValues, bottom=bottoms, **attrs)
             
-            bottoms = [bar.yValues[i] + bottoms[i] \
-                           for i in xrange(len(self.xTickLabelPoints))]
+            currHandle = axis.bar(self.xValues, bar.yValues, bottom=bottoms, 
+                                  **attrs)
+            
+            bottoms = [bar.yValues[i] + bottoms[i] 
+                       for i in xrange(len(self.xValues))]
             
             plotHandles.append(currHandle[0])
             plotLabels.append(bar.label)
-
+        
         plotHandles.reverse()
         plotLabels.reverse()
         return [plotHandles, plotLabels]
