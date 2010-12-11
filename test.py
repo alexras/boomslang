33d5df712897f0b694b5087d0865ace0baa2033d
Alex Rasmussen <alexras@acm.org>
2010-12-10 16:14:13 -0800

    Using PIL to make the tests do an image diff with reference images during testing.

--- test.py
+++ test.py
@@ -1,13 +1,31 @@
 import unittest
 import os
+from PIL import Image
+from PIL import ImageChops
 
-class TestExamples(unittest.TestCase):
-    for example in filter(lambda x: x.endswith('py'), os.listdir('examples')):
-        example_no_py = example[:-3]
-        full_path = os.path.join('examples', example)
-        exec("def test_%s(self):\n" % (example_no_py,) +
-             "    res = os.system('python %s')\n" % (full_path,) +
-             "    self.assertTrue(res == 0)")
+class TestExamples(unittest.TestCase):        
+    def assert_images_same(self, im1_path, im2_path):
+        im1 = Image.open(im1_path)
+        im2 = Image.open(im2_path)
+
+        diff = ImageChops.difference(im2,im1).getbbox()
+        
+        self.assertTrue(diff == None, "Images differ; bounding box of "
+                        "difference is %s" % (diff,))
+
+    def test_vs_reference(self):
+        for example in filter(lambda x: x.endswith('py'), 
+                              os.listdir('examples')):
+            example_no_py = example[:-3]
+            full_path = os.path.join('examples', example)
+            result_path = example_no_py + ".png"
+            reference_path = os.path.join('examples', 'reference', 
+                                          result_path)
+            
+            res = os.system("python %s" % (full_path))
+            self.assertTrue(res == 0)
+            self.assert_images_same(result_path, reference_path)
+            os.unlink(result_path)
 
 if __name__ == '__main__':
     unittest.main()
