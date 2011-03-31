5535dbf82bcbc42933502e1f4184b17539aabb7c
Alex Rasmussen <alexras@acm.org>
2011-03-31 11:28:39 -0700

    Image comparison test base class.

---
+++ examples/ImageComparisonTestCase.py
@@ -0,0 +1,45 @@
+from PIL import Image
+from PIL import ImageChops
+from abc import ABCMeta,abstractmethod
+import os
+import unittest
+
+from matplotlib import rc
+
+class ImageComparisonTestCase(unittest.TestCase):
+    __metaclass__ = ABCMeta
+
+    scriptPath = os.path.abspath(os.path.dirname(__file__))
+
+    def __init__(self, imageName):
+        super(ImageComparisonTestCase,self).__init__('testImageComparison')
+        self.imageName = imageName
+
+    def setUp(self):
+        if os.path.exists(self.imageName):
+            os.unlink(self.imageName)
+
+    # def tearDown(self):
+    #     if os.path.exists(self.imageName):
+    #         os.unlink(self.imageName)
+
+    @abstractmethod
+    def constructImage(self):
+        pass
+
+    def testImageComparison(self):
+        rc('font', family="serif", serif="Arial",
+           monospace="Courier, Computer Modern Typewriter")
+
+        self.constructImage()
+
+        referenceImage = os.path.abspath(os.path.join(
+                self.scriptPath, "reference", self.imageName))
+
+        ourImage = Image.open(self.imageName)
+        refImage = Image.open(referenceImage)
+
+        diff = ImageChops.difference(refImage, ourImage).getbbox()
+
+        self.assertTrue(diff == None, "Image differs from reference; "
+                        "bounding box of difference is %s" % (diff,))
