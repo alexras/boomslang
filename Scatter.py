583256618e88e589fba51967b1dfcc8b307eb0d1
Alex Rasmussen <alexras@acm.org>
2011-03-31 11:26:28 -0700

    Adding missing kwargs

--- Scatter.py
+++ Scatter.py
@@ -6,19 +6,19 @@ class Scatter(PlotInfo):
     def __init__(self,
                  marker='o',
                  markerSize=20,
-                 color="black"):
+                 color="black", **kwargs):
         super(Scatter,self).__init__("scatter", **kwargs)
         self.marker = marker
         self.markerSize = markerSize
         self.color = color
-            
+
     def draw(self, axis):
         kwdict = {}
         kwdict["marker"] = self.marker
         kwdict["label"] = self.label
         kwdict["color"] = self.color
         kwdict["s"] = self.markerSize
-        
-        return [[axis.scatter(self.xValues, self.yValues, **kwdict)], 
+
+        return [[axis.scatter(self.xValues, self.yValues, **kwdict)],
                 [self.label]]
-    
+
