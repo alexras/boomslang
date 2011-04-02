42c79fa108fd4dc02aa5e3fdd712e5af787e5fab
Alex Rasmussen <alexras@acm.org>
2011-04-02 12:20:05 -0700

    Replacing print statements with appropriate warnings and integrating the StepType data descriptor into Line.

--- Line.py
+++ Line.py
@@ -1,8 +1,12 @@
 import pylab
 from matplotlib import pyplot
 from PlotInfo import PlotInfo
+from StepType import StepType
+import warnings
 
 class Line(PlotInfo):
+    steps = StepType("steps")
+
     def __init__(self,
                  color='black',
                  width=1,
@@ -11,7 +15,7 @@ class Line(PlotInfo):
                  markerSize=8.0,
                  dates=False,
                  loglog=False,
-                 steps=False,
+                 steps=None,
                  alpha=None,
                  antialiased=False,
                  **kwargs
@@ -32,21 +36,17 @@ class Line(PlotInfo):
         self.antialiased = antialiased
 
     def stepFunction(self, stepType="pre"):
-        validStepTypes = ["pre", "mid", "post"]
-        
-        if stepType not in validStepTypes:
-            print >>sys.stderr, "%s is not a valid step type. Valid step types are %s" % (stepType, ", ".join(validStepTypes))
-            sys.exit(1)
-        
         self.steps = stepType
 
     def draw(self, axis):
         super(Line, self).draw(axis)
-        
+
         if self.dates:
             plotFunc = axis.plot_date
         elif self.loglog:
-            print >>sys.stderr, "Setting loglog in Lines will be deprecated soon. Set this in Plot instead."
+            warnings.warn("Setting loglog in Lines will be deprecated soon. "
+                          "Set this in Plot instead.",
+                          warnings.PendingDeprecationWarning)
             plotFunc = axis.loglog
         else:
             plotFunc = axis.plot
@@ -56,10 +56,10 @@ class Line(PlotInfo):
         kwdict["color"] = self.color
         kwdict["label"] = self.label
         kwdict["linewidth"] = self.lineWidth
-        
+
         if self.steps is not None:
             kwdict["drawstyle"] = "steps-%s" % (self.steps)
-        
+
         if self.marker is not None:
             kwdict["marker"] = self.marker
             kwdict["markersize"] = self.markerSize
@@ -70,5 +70,5 @@ class Line(PlotInfo):
             kwdict["antialiased"] = self.antialiased
         if self.alpha is not None:
             kwdict["alpha"] = self.alpha
-        
+
         return [[plotFunc(self.xValues, self.yValues, **kwdict)], [self.label]]
