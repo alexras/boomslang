5f0edef906f61129bdf19c55c775a1fecfef063d
Alex Rasmussen <alexras@acm.org>
2011-04-02 12:09:22 -0700

    Making Location a data descriptor for ease of access.

--- Location.py
+++ Location.py
@@ -12,7 +12,21 @@ class Location(object):
         "upper center" : ["top middle", "upper middle"],
         "center" : ["middle"]
      }
-    
+
+
+    # Translates location strings into the integers that Matplotlib expects
+    LOCATION_TO_INT = {"best" : 0,
+                       "upper right" : 1,
+                       "upper left" : 2,
+                       "lower left" : 3,
+                       "lower right" : 4,
+                       "right" : 5,
+                       "center left" : 6,
+                       "center right" : 7,
+                       "lower center" : 8,
+                       "upper center" : 9,
+                       "center" : 10}
+
     VALID_LOCATIONS = {}
 
     for (glyph, equivalents) in _LOCATION_TRANSLATOR.items():
@@ -21,14 +35,17 @@ class Location(object):
         for equiv in equivalents:
             VALID_LOCATIONS[equiv] = glyph
 
-    def __init__(self):
-        self._location = "best"
+    def __init__(self, name):
+        self.name = name
 
-    @property
-    def location(self):
-        return self._location
+    def __set__(self, instance, value):
+        if value not in Location.VALID_LOCATIONS:
+            raise TypeError("'%s' is not a valid legend location" % value)
 
-    @location.setter
-    def location(self, value):
-        assert value in VALID_LOCATIONS, "'%s' is not a valid legend location"
-        self._location = VALID_LOCATIONS[value]
+        instance.__dict__[self.name] = Location.VALID_LOCATIONS[value]
+
+    def __get__(self, instance, owner):
+        if self.name not in instance.__dict__:
+            raise AttributeError, self.name
+
+        return Location.LOCATION_TO_INT[instance.__dict__[self.name]]
