cad10d5bddfaca1ce2a723010a35f6527e206834
Alex Rasmussen <alexras@acm.org>
2011-04-02 12:18:35 -0700

    Data descriptor for step types.

---
+++ StepType.py
@@ -0,0 +1,19 @@
+class StepType(object):
+    validStepTypes = ["pre", "mid", "post", None]
+
+    def __init__(self, name):
+        self.name = name
+
+    def __set__(self, instance, value):
+        if value not in StepType.validStepTypes:
+            raise ValueError(
+                "%s is not a valid step type. Valid step types are %s" \
+                    % (value, ", ".join(StepType.validStepTypes)))
+
+        instance.__dict__[self.name] = value
+
+    def __get__(self, instance, owner):
+        if self.name not in instance.__dict__:
+            raise AttributeError, self.name
+
+        return instance.__dict__[self.name]
