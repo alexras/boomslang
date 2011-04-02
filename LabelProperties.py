f8bb9948b6ec55e5bd88824bad7cc6efd08eb0a1
Alex Rasmussen <alexras@acm.org>
2011-04-02 12:08:40 -0700

    Making LabelProperties usable by defining a couple extra interfaces.

--- LabelProperties.py
+++ LabelProperties.py
@@ -2,29 +2,36 @@ import collections
 import sys
 
 class LabelProperties(collections.MutableMapping):
-    VALID_PROPERTIES = ["alpha", 
-                        "backgroundColor", 
-                        "color", 
-                        "horizontalalignment", 
+    VALID_PROPERTIES = ["alpha",
+                        "backgroundColor",
+                        "color",
+                        "horizontalalignment",
                         "linespacing",
-                        "multialignment", 
-                        "rotation", 
-                        "stretch", 
-                        "style", 
-                        "verticalalignment", 
+                        "multialignment",
+                        "rotation",
+                        "stretch",
+                        "style",
+                        "verticalalignment",
                         "weight"]
-    
+
     def __init__(self):
         self._props = {}
-        
+
     def __getitem__(self, key):
         return self._props[key]
-    
+
     def __setitem__(self, key, value):
-        assert key in VALID_PROPERTIES, ("Label property '%s' is not "
-                                         "currently supported") % (key)
+        if key not in LabelProperties.VALID_PROPERTIES:
+            raise ValueError("Label property '%s' is not currently supported"\
+                                 % (key))
         self._props[key] = value
-            
+
     def __delitem__(self, key):
         del self._props[key]
-    
+
+
+    def __iter__(self):
+        return self._props.__iter__()
+
+    def __len__(self):
+        return len(self._props)
