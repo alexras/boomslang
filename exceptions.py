0777ff8253da76db16474e93d500efd1cac26e79
Alex Rasmussen <alexras@acm.org>
2011-04-02 12:07:28 -0700

    A couple of Boomslang-related exceptions

---
+++ exceptions.py
@@ -0,0 +1,10 @@
+class BoomslangPlotRenderingException(Exception):
+    """
+    Thrown when Boomslang fails when rending plots.
+    """
+    pass
+
+class BoomslangPlotConfigurationException(Exception):
+    """
+    Thrown when a plot is being misconfigured.
+    """
